39bb88f7d3fd15afb41343ee1f6f6c231f13963d
Winterhart <m.user.work@gmail.com>
2016-11-09 20:08:00 -0500

    Change of multiple Path

--- public/view/main/Secured/Account.php
+++ public/view/main/Secured/Account.php
@@ -3,12 +3,12 @@
 <head>
 	<title> Account </title>
 	<!-- This section is for the Head -->
-	<?php include($_SERVER['DOCUMENT_ROOT']. 'public/view/include/Head.php');?>
+	<?php include($_SERVER['DOCUMENT_ROOT']. '/comp353-project/public/view/include/Head.php');?>
 </head>
 <body>
 	<!-- Page Content -->
 	<!-- This Section is for the Navigation file -->
-	<?php include($_SERVER['DOCUMENT_ROOT']. 'public/view/include/Header.php');?>
+	<?php include($_SERVER['DOCUMENT_ROOT']. '/comp353-project/public/view/include/Header.php');?>
     <!-- INCLUDE CONTENT OF PAGE HERE -->
     <div id="page-content-wrapper">
     <p> Account page COMP353</p>
@@ -16,7 +16,7 @@
     <!-- END OF CONTENT --> 
 	<div><?php echo $this_page; ?></div>
 	<!-- This Section is for the footer -->
-	<?php include($_SERVER['DOCUMENT_ROOT']. 'public/view/include/Footer.php');?>
+	<?php include($_SERVER['DOCUMENT_ROOT']. '/comp353-project/public/view/include/Footer.php');?>
 	
 </body>
 </html>
