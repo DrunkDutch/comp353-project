e7ffb292b9f0c25ff52873790fc6fa99e4600129
Winterhart <m.user.work@gmail.com>
2016-11-12 16:48:21 -0500

    Login function done, dbMakeConnection

--- public/view/main/LOG_IN.php
+++ public/view/main/LOG_IN.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
+<<<<<<< HEAD
     <title> Sign In </title>
     <!-- This section is for the Head -->
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/comp353-project/public/view/include/Head.php'); ?>
+=======
+	<title> Sign In </title>
+	<!-- This section is for the Head -->
+	<?php include($_SERVER['DOCUMENT_ROOT']. '/comp353-project/public/view/include/Head.php');?>
+
+>>>>>>> Login function done, dbMakeConnection
 </head>
 
 <body>
+<<<<<<< HEAD
 <!-- Page Content -->
 <!-- This Section is for the Navigation file -->
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/comp353-project/public/view/include/Header.php'); ?>
@@ -15,6 +23,32 @@
     <div class="row">
         <div class="text-center"><i class="fa fa-car fa-5x" aria-hidden="true"></i>
         </div>
+=======
+	<!-- Page Content -->
+	<!-- This Section is for the Navigation file -->
+<?php include($_SERVER['DOCUMENT_ROOT']. '/comp353-project/public/view/include/Header.php');?>
+    <!-- INCLUDE CONTENT OF PAGE HERE -->
+    <div id="page-content-wrapper">
+	<div class="row"><div class="text-center"><i class="fa fa-car fa-5x" aria-hidden="true"></i>
+</div></div>
+<?php include "../app/login_function.php"?>	
+    <form class="container-fluid col-md-4 col-md-offset-4" action="/comp353-project/app/login_function.php" method="POST">
+	
+	<div class="form-group">
+    <label for="Email">Email</label>
+    <input type="email" name="email" class="form-control" aria-describedby="emailHelp" id="exampleInputEmail1" placeholder="Enter Email">
+    </div>
+    <div class=form-group">
+    <label> OR </label>
+    </div>
+    <div class=form-group>
+	<label for="username">Username</label>
+    <input type="text" name="user" class="form-control" aria-describedby="UsernameHelp"  id="exampleInputEmail1" placeholder="Enter Username">
+    </div>
+    <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+>>>>>>> Login function done, dbMakeConnection
     </div>
     <?php include "../app/login_function.php" ?>
     <h1>Log In</h1>
