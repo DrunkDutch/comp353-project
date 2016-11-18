bf816cb0e6172cbd6de130a57620d71fec362314
Winterhart <m.user.work@gmail.com>
2016-11-17 23:29:03 -0500

    Ride.php 20% completed...

--- public/view/main/Secured/Rides.php
+++ public/view/main/Secured/Rides.php
@@ -1,22 +1,11 @@
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title> Rides </title>
 	<!-- This section is for the Head -->
 	<?php include($_SERVER['DOCUMENT_ROOT']. '/comp353-project/public/view/include/Head.php');?>
-    <script>
-        function showRides() {
-            xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    document.getElementById("rides").innerHTML = this.responseText;
-                }
-            };
-            xmlhttp.open("GET","/comp353-project/app/getRides.php",true);
-            xmlhttp.send();
-        }
-        showRides();
-    </script>
+
 </head>
 <body>
 	<!-- Page Content -->
@@ -24,9 +13,43 @@
 	<?php include($_SERVER['DOCUMENT_ROOT']. '/comp353-project/public/view/include/Header.php');?>
     <!-- INCLUDE CONTENT OF PAGE HERE -->
     <div id="page-content-wrapper">
-    <h1>All Rides</h1>
+    <h1>Current Rides</h1>
+            <div class="container" style="border-style:solid; border-width:3px; height:90%; overflow-y:scroll;" id="rides">  
+<?php 
 
-            <div id="rides"></div>
+function GetDataForRide(){
+	include($_SERVER['DOCUMENT_ROOT']. '/comp353-project/config/dbMakeConnection.php');
+	
+	$status = Connected();
+		if($status == 1){
+		try{
+			$d = new dbMakeConnection;
+		}
+		
+		catch(PDOException $e){ echo($e);}
+
+		$stmt = $d->conn->prepare("SELECT * FROM comp353.Ride");
+        	$stmt->execute();
+		$result = $stmt->fetchAll();
+		
+		
+		foreach($result as&$val){
+		
+		$r = $val["Date"];
+		$t = $val["DepartTime"];
+		echo '<div class="row" style="height:150px;border-style:solid; border-width:3px;"><p style="margin-top:20px;">Destination:&nbsp</p><p>Departure time:&nbsp'.$r.'&nbspat:&nbsp'.$t. '&nbsp</p><a href="#"><button class="btn btn-success">Get Details</button></a></div>';
+		}
+
+		
+	}
+
+}
+GetDataForRide();
+
+?>
+           
+<div class="row" style="height:150px;border-style:solid; border-width:3px;"><p>Destination:&nbsp</p><p>Departure time:&nbsp2016-11-18&nbspat:&nbsp09:00:00&nbsp</p><a href="#"><button class="btn btn-success">Get Details</button></a></div>
+	</div>
 
     </div>
     <!-- END OF CONTENT -->
