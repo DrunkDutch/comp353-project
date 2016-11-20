3f15d3151668fb90bc2a66fc95a8bc291383f4b0
Stella Lee <slee.mcin@gmail.com>
2016-11-20 15:46:05 -0500

    initial

--- public/view/main/Secured/Messages.php
+++ public/view/main/Secured/Messages.php
@@ -1,27 +1,100 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> Sign In </title>
+	<title> Review </title>
 	<!-- This section is for the Head -->
-	<?php include($_SERVER['DOCUMENT_ROOT']. '/comp353-project/public/view/include/Head.php');?>
-
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/comp353-project/public/view/include/Head.php'); ?>
 </head>
 <body>
-	<!-- Page Content -->
-	<!-- This Section is for the Navigation file -->
-<?php include($_SERVER['DOCUMENT_ROOT']. '/comp353-project/public/view/include/Header.php');?>
-    <!-- INCLUDE CONTENT OF PAGE HERE -->
-    <div id="page-content-wrapper">
-	<div class="row"><div class="text-center"><i class="fa fa-envelope-o fa-5x" aria-hidden="true"></i>
-</div></div>	
-    
+<!-- Page Content -->
+<!-- This Section is for the Navigation file -->
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/comp353-project/public/view/include/Header.php'); ?>
+<!-- INCLUDE CONTENT OF PAGE HERE -->
+<div id="page-content-wrapper">
+	<h1>My Messages</h1>
+	<div class="row">
+		<div class="text-center">
+			<i class="fa fa-commenting fa-5x" aria-hidden="true"></i>
+		</div>
+	</div>
+	<div class="container" style="border-style:solid; border-width:3px; height:90%; overflow-y:scroll;" id="messages">
 
-    </div>
-    <!-- END OF CONTENT --> 
-	<div><?php echo $this_page; ?></div>
-	<!-- This Section is for the footer -->
-	<?php include($_SERVER['DOCUMENT_ROOT']. '/comp353-project/public/view/include/Footer.php');?>
-	
+
+
+
+		<?php
+
+		function GetDetailedMessage($id){
+			$status = Connected();
+			if($status == 1){
+				try{
+					$d = new dbMakeConnection;
+				}
+				catch(PDOException $e){ echo($e);}
+				$stmt = $d->conn->prepare("SELECT * FROM comp353.Location WHERE LocationId = :id");
+				$stmt->bindParam(':id', $id);
+				$stmt->execute();
+				$result = $stmt->fetch(PDO::FETCH_ASSOC);
+				$AllAddress = ' '. $result['StreetNum'] . ' '. $result['Street'] .' '. $result['City'];
+				return $AllAddress;
+			}
+		}
+
+
+		function GetMessages() {
+			include($_SERVER['DOCUMENT_ROOT'] . '/comp353-project/config/dbMakeConnection.php');
+
+			$status = Connected();
+			if ($status == 1) {
+				try {
+					$d = new dbMakeConnection;
+				} catch (PDOException $e) {
+					echo($e);
+				}
+
+				$u = $_SESSION['username'];
+
+				$stmt = $d->conn->prepare(
+					"select SenderId, ReceiverId, `Date`, Content
+                    from Message join Member on Message.ReceiverId = Member.UserId
+                    where Member.UName like :u"
+				);
+				$stmt->bindParam(':u', $u);
+				$stmt->execute();
+				$result = $stmt->fetchAll();
+
+				foreach($result as&$val){
+					$Rid = $val["SenderId"];
+					$Did = $val["ReceiverId"];
+					$AllAdd = GetDetailedMessage($Did);
+					$r = $val["Date"];
+					$t = $val["Content"];
+					// Build URL For each Button...
+					$url = "http://" . $_SERVER['SERVER_NAME'] .   $_SERVER[''].substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/')) . '/Rides-Details.php?id=' .$Rid ;
+					// Create HTML...
+					echo '<div class="row" style="height:150px;border-style:solid; border-width:3px;"><p style="margin-top:20px;">Destination:' .$AllAdd. '&nbsp</p><p>Departure time:&nbsp'.$r.'&nbspat:&nbsp'.$t. '&nbsp</p><a href="'.$url.'"><button class="btn btn-success">Get Details</button></a></div>';
+				}
+			}
+		}
+
+		GetMessages();
+		?>
+
+		<!--        <div class="row" style="height:150px;border-style:solid; border-width:3px;">-->
+		<!--            <p>Sender: </p>-->
+		<!--            <p>Content: </p>-->
+		<!--            <p>Date: </p>-->
+		<!--            <a href="#">-->
+		<!--                <button class="btn btn-success">Get Details</button>-->
+		<!--            </a>-->
+		<!--        </div>-->
+	</div>
+
+</div>
+<!-- END OF CONTENT -->
+<div><?php echo $this_page; ?></div>
+<!-- This Section is for the footer -->
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/comp353-project/public/view/include/Footer.php'); ?>
+
 </body>
 </html>
-
