d2017af635b80fbe64a017a3fad5ba5cfdbd8df4
Stella Lee <slee.mcin@gmail.com>
2016-11-20 17:57:17 -0500

    add group messages

--- public/view/main/Secured/Messages.php
+++ public/view/main/Secured/Messages.php
@@ -30,10 +30,32 @@
 
 				$u = $_SESSION['username'];
 
+				// GET USER MESSAGES
 				$stmt = $d->conn->prepare(
 					"select m.MessageId, mem2.UName, m.Date, m.Content 
 					from comp353.Message m join comp353.Member mem2 on comp353.m.SenderId = comp353.mem2.UserId 
 					join comp353.Member mem on comp353.m.ReceiverId = comp353.mem.UserId 
+					where comp353.mem.UName like :u");
+				$stmt->bindParam(':u', $u);
+				$stmt->execute();
+				$result = $stmt->fetchAll();
+
+				foreach($result as&$val){
+					$mId = $val["MessageId"];
+					$sId = $val["UName"];
+					$date = $val["Date"];
+					$content = $val["Content"];
+					// Build URL For each Button...
+					$url = "http://" . $_SERVER['SERVER_NAME'] .   $_SERVER[''].substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/')) . '/Message-Details.php?id=' .$mId ;
+					// Create HTML...
+					echo '<div class="row" style="height:150px;border-style:solid; border-width:3px;"><p style="margin-top:20px;">Sender:&nbsp'.$sId.'</p><p>Date:&nbsp'.$date. '&nbsp</p><p>Content:&nbsp'.$content. '&nbsp</p><a href="'.$url.'"><button class="btn btn-success">Get Details</button></a></div>';
+				}
+
+				// GET USER GROUP MESSAGES
+				$stmt = $d->conn->prepare(
+					"select m.MessageId, mem2.UName, m.Date, m.Content 
+					from comp353.Message m join comp353.Member mem2 on comp353.m.SenderId = comp353.mem2.UserId 
+					join comp353.Member mem on comp353.m.ReceiverId = comp353.Member.Privilege
 					where comp353.mem.UName like :u");
 				$stmt->bindParam(':u', $u);
 				$stmt->execute();
