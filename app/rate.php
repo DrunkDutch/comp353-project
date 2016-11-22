d5b9931649b710afd26e3b3916f8b883a9d9baf7
Stella Lee <slee.mcin@gmail.com>
2016-11-22 12:18:45 -0500

    rate queries

--- app/rate.php
+++ app/rate.php
@@ -1,15 +1,16 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT']. '/comp353-project/config/config.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/comp353-project/config/config.php');
 include($_SERVER['DOCUMENT_ROOT'] . '/comp353-project/config/dbMakeConnection.php');
 
 $username = $_POST['user'];
 $score = $_POST['score'];
+$rideId = $_GET['id'];
 
 if (!((empty($username)) and (empty($score)))) {
-    Rate($username, $score);
+    Rate($username, $score, $rideId);
 }
 
-function Rate($t, $m)
+function Rate($ratee, $score, $rideId)
 {
     $status = Connected();
 
@@ -38,11 +39,25 @@ function Rate($t, $m)
             return;
         }
 
-//        $stmt = $d->conn->prepare(/* INSERT RATING */);
-//        $stmt->bindParam(':s', $s['UserId']);
-//        $stmt->bindParam(':r', $r['UserId']);
-//        $stmt->bindParam(':c', $m);
-//        $stmt->execute();
+        $stmt = $d->conn->prepare("select * from `comp353`.`Rating` where RideId = :id and RaterId = :r and RateeId = :e");
+        $stmt->bindParam(':id', $rideId);
+        $stmt->bindParam(':r', $u);
+        $stmt->bindParam(':e', $ratee);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (empty($result)) {
+
+            $stmt = $d->conn->prepare("INSERT INTO `comp353`.`Rating`(`RideId`,`RaterId`,`RateeId`, `Score`)VALUES(:id,:r,:e,:s)");
+            $stmt->bindParam(':id', $rideId);
+            $stmt->bindParam(':r', $u);
+            $stmt->bindParam(':e', $ratee);
+            $stmt->bindParam(':s', $score);
+            $stmt->execute();
+        }
+        else {
+            echo "Already rated for this ride.";
+        }
 
         header("Location: http://localhost/comp353-project/public/view/main/Secured/SentMessages.php");
     }
