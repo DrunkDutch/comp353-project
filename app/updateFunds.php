9ad507e92a5ae8ffca4854056cc231aac048e1a9
Stella Lee <slee.mcin@gmail.com>
2016-11-21 21:20:45 -0500

    add balances functionality

---
+++ app/updateFunds.php
@@ -0,0 +1,55 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . '/comp353-project/config/config.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/comp353-project/config/dbMakeConnection.php');
+
+$funds = $_POST['funds'];
+
+if (!empty($funds)) {
+    UpdateFunds($funds);
+}
+Redirect();
+
+function Redirect()
+{
+    $url = "http://" . $_SERVER['SERVER_NAME'] . '/comp353-project/public/view/main/Secured/Account.php?id=' . $_SESSION['UserId'];
+    header("Location:" . $url . " ");
+}
+
+// Check Login with DB assuming variable passed in are cleaned and not all empty
+function UpdateFunds($funds)
+{
+    $status = Connected();
+    if ($status == 1) {
+        try {
+            $d = new dbMakeConnection;
+        } catch (PDOException $e) {
+            echo($e);
+        }
+
+        $u = $_SESSION['username'];
+
+        // adding funds
+        if ($funds > 0) {
+            $stmt = $d->conn->prepare(/* UPDATE BALANCE */);
+            $stmt->bindParam(':u', $u);
+            $stmt->execute();
+        } else {
+            // check if sufficient funds
+            $stmt = $d->conn->prepare("SELECT Balance FROM comp353.Member WHERE UName = :u");
+            $stmt->bindParam(':u', $u);
+            $stmt->execute();
+
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            $balance = $result['Balance'];
+
+            $toRemove = abs($funds);
+            if ($balance = $toRemove < 0) {
+                $toRemove = $balance;
+            }
+
+            $stmt = $d->conn->prepare(/* UPDATE BALANCE */);
+            $stmt->bindParam(':u', $u);
+            $stmt->execute();
+        }
+    }
+}
--- public/view/main/Secured/Account.php
+++ public/view/main/Secured/Account.php
@@ -60,7 +60,7 @@
         if (empty($accountDetails)) {
             echo 'User not found';
         } else {
-            $url = "http://" . $_SERVER['SERVER_NAME'] .   $_SERVER[''].substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/')) . '/Ratings-Details.php?id=' .$accountDetails['UserId'] ;
+            $url = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER[''] . substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/')) . '/Ratings-Details.php?id=' . $accountDetails['UserId'];
 
             echo '<div class="row">Username:&nbsp' . $accountDetails['UName'] . '</div>
             <div class="row">UserID:&nbsp' . $accountDetails['UserId'] . '</div>
@@ -69,17 +69,29 @@
 		        <div class="row">DOB:&nbsp' . $accountDetails['DOB'] . '</div>
 		        <div class="row">Phone:&nbsp' . $accountDetails['Phone'] . '</div>
 		        <br/>
-		        <a href="'.$url.'"><button class="btn btn-success">See Rating</button></a>';
+		        <a href="' . $url . '"><button class="btn btn-success">See Rating</button></a>';
 
             $privilege = $_SESSION['privi'];
             // if own account or if you are root or admin
             if ($_SESSION['UserId'] == $accountDetails['UserId'] || $privilege <= 2) {
+
+                // add buttons
+                echo '&nbsp;<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal1">Account Balance</button>';
                 $url = "http://" . $_SERVER['SERVER_NAME'] . '/comp353-project/app/deleteAccount.php?id=' . $accountDetails['UserId'];
+                echo '&nbsp;<button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal2">Edit Account</button>&nbsp;<a href="' . $url . '"><button class="btn btn-danger">Delete Account</button></a>';
 
-                echo '&nbsp;<button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal">Edit Account</button>&nbsp;<a href="' . $url . '"><button class="btn btn-danger">Delete Account</button></a>';
+                // able to view balance
+                include "/comp353-project/app/updateFunds.php";
+                $balance = $accountDetails['Balance'];
+                echo '<div id="myModal1" class="modal fade" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">Account Balance</h4></div>';
+                echo '<div class="modal-body"><form action="/comp353-project/app/updateFunds.php" method="POST">';
+                echo '<div class="form-group"><label for="Username">Current Balance: ' . $balance . '</label>';
+                echo '<div class="form-group"><label for="funds">Add Funds</label><input type="text" name="funds" class="form-control" aria-describedby="funds" id="funds" placeholder="100"></div>';
+                echo '<button class="btn btn-default" data-dismiss="modal">Close</button>&nbsp;<button type="submit" class="btn btn-success">Submit</button></form></div></div></div>';
 
+                // able to edit and delete account
                 include "/comp353-project/app/editAccount.php";
-                echo '<div id="myModal" class="modal fade" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">Edit Account</h4></div>';
+                echo '<div id="myModal2" class="modal fade" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">Edit Account</h4></div>';
                 echo '<div class="modal-body"><form action="/comp353-project/app/editAccount.php" method="POST"><div class="form-group"><label for="Username">Username</label><input type="text" name="username" class="form-control" aria-describedby="UsernameHelp" id="exampleInputUsername1" placeholder="Username"></div>';
                 echo '<div class="form-group"><label for="FirstName">First Name</label><input type="text" name="FirstName" class="form-control" aria-describedby="FirstnameHelp" id="exampleInputFirstName1" placeholder="First Name"></div>';
                 echo '<div class="form-group"><label for="LastName">Last Name</label><input type="text" name="LastName" class="form-control" aria-describedby="LastnameHelp" id="exampleInputLastName1" placeholder="Last Name"></div>';
@@ -93,10 +105,8 @@
                 if ($privilege <= 2) {
                     echo '<div class="form-group"><label for="exampleInputActive">Active</label><input type="text" name="active" class="form-control" aria-describedby="ActiveHelp" id="exampleInputActive" placeholder="1"></div>';
                     echo '<div class="form-group"><label for="exampleInputPrivilege">Privilege</label><input type="text" name="privilege" class="form-control" aria-describedby="PrivilegeHelp" id="exampleInputPrivilege" placeholder="1"></div>';
-
                 }
-
-                    echo '<button class="btn btn-default" data-dismiss="modal">Close</button><button type="submit" class="btn btn-success">Submit</button></form></div></div></div></div>';
+                echo '<button class="btn btn-default" data-dismiss="modal">Close</button><button type="submit" class="btn btn-success">Submit</button></form></div></div></div>';
 
 
             }
@@ -104,6 +114,7 @@
         }
         echo '</div>';
     }
+
     CollectResult();
     ?>
     <!-- END OF CONTENT -->
