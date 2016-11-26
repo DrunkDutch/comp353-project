d1cd2355e97b5591c0ff5da85db4dec4a7e6fb07
Stella Lee <slee.mcin@gmail.com>
2016-11-26 16:51:46 -0500

    reset user force fields

--- public/view/main/ResetUser.php
+++ public/view/main/ResetUser.php
@@ -11,7 +11,7 @@
 <?php include("../include/Header.php"); ?>
 <!-- INCLUDE CONTENT OF PAGE HERE -->
 <div id="page-content-wrapper">
-    <?php include "../app/resetAccount.php" ?>
+    <?php include "../../../app/resetAccount.php" ?>
     <h1>Reset Username and Password</h1>
     <form class="container-fluid col-md-4 col-md-offset-4" action="/comp353-project/app/resetAccount.php"
           method="POST">
@@ -19,12 +19,12 @@
         <div class=form-group>
             <label for="username">New Username</label>
             <input type="text" name="user" class="form-control" aria-describedby="UsernameHelp" id="exampleInputEmail1"
-                   placeholder="Enter New Username">
+                   placeholder="Enter New Username" required="required">
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">New Password</label>
             <input type="password" name="password" class="form-control" id="exampleInputPassword1"
-                   placeholder="Enter New Password">
+                   placeholder="Enter New Password" required="required">
         </div>
         <input type="submit" value="Reset" class="btn btn-primary">
     </form>
