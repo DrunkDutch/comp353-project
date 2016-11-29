907b19dafefcb099b3db2d68b798265c27048bf5
Stella Lee <slee.mcin@gmail.com>
2016-11-29 13:49:47 -0500

    review queries updated

--- public/view/main/Secured/Reviews.php
+++ public/view/main/Secured/Reviews.php
@@ -58,13 +58,39 @@ include($_SERVER['DOCUMENT_ROOT'] . '/comp353-project/config/dbMakeConnection.ph
                     echo($e);
                 }
 
-                // GET USER MESSAGES
-                $stmt = $d->conn->prepare("SELECT * FROM ".$GLOBALS['db_name'].".Ride WHERE Date < CURDATE()");
+                $u = $_SESSION['UserId'];
+
+                // GET Rider rides
+                $stmt = $d->conn->prepare("SELECT Ride.RideId from ".$GLOBALS['db_name'].".Ride join ".$GLOBALS['db_name'].".Rider on Ride.RideId = RiderId where Rider.RiderId =:u");
+                $stmt->bindParam(':u', $u);
                 $stmt->execute();
                 $result = $stmt->fetchAll();
 
                 if (empty($result)) {
-                    echo "No past rides";
+                    echo "No past rides as rider";
+                }
+                else {
+                    foreach ($result as &$val) {
+                        $Rid = $val["RideId"];
+                        $Did = $val["DestinationId"];
+                        $AllAdd = GetDetailAddress($Did);
+                        $r = $val["Date"];
+                        $t = $val["DepartTime"];
+                        // Build URL For each Button...
+                        $url = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER[''] . substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/')) . '/Rides-Details.php?id=' . $Rid;
+                        // Create HTML...
+                        echo '<div class="row" style="height:150px;border-style:solid; border-width:3px;"><p style="margin-top:20px;">Destination:' .$AllAdd. '&nbsp</p><p>Departure time:&nbsp'.$r.'&nbspat:&nbsp'.$t. '&nbsp</p><a href="' . $url . '"><button class="btn btn-success">Rate Ride</button></a></div>';
+                    }
+                }
+
+                // GET Driver rides
+                $stmt = $d->conn->prepare("SELECT Ride.RideId from ".$GLOBALS['db_name'].".Ride join ".$GLOBALS['db_name'].".Driver on Ride.RideId=DriverId where DriverId =:u");
+                $stmt->bindParam(':u', $u);
+                $stmt->execute();
+                $result = $stmt->fetchAll();
+
+                if (empty($result)) {
+                    echo "No past rides as driver";
                 }
                 else {
                     foreach ($result as &$val) {
